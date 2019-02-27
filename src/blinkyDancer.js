6ede4b138967a972a94a73119e737eebbe3a923f
yidahc <yidah.a.curiel@gmail.com>
2019-02-26 19:37:19 -0600

    alsmost done rewriting code to be pseudoclassical

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,19 +1,17 @@
-var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
-  var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
-
+var BlinkyDancer = function(top, left, timeBetweenSteps) {
+  makeDancer.call(this, top, left, timeBetweenSteps);
+};
+BlinkyDancer.prototype = Object.create(makeDancer.prototype);
+BlinkyDancer.prototype.constructor = BlinkyDancer; 
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
+var oldStep = BlinkyDancer.step;
 
-  var oldStep = blinkyDancer.step;
-
-  blinkyDancer.step = function() {
+BlinkyDancer.prototype.step = function() {
     // call the old version of step at the beginning of any call to this new version of step
     oldStep();
     // toggle() is a jQuery method to show/hide the <span> tag.
     // See http://api.jquery.com/category/effects/ for this and
     // other effects you can use on a jQuery-wrapped html tag.
-    blinkyDancer.$node.toggle();
-  };
-
-  return blinkyDancer;
+    this.$node.toggle();
 };
--- src/dancer.js
+++ src/dancer.js
@@ -1,32 +1,30 @@
 // Creates and returns a new dancer object that can step
 var makeDancer = function(top, left, timeBetweenSteps) {
 
-  var dancer = {};
-
+  this.dancer = {};
   // use jQuery to create an HTML <span> tag
-  dancer.$node = $('<span class="dancer"></span>');
-
-  dancer.step = function() {
+  this.dancer.$node = $('<span class="dancer"></span>');
+};
+  
+  makeDancer.prototype.step = function() {
     // the basic dancer doesn't do anything interesting at all on each step,
     // it just schedules the next step
-    setTimeout(dancer.step, timeBetweenSteps);
+    setTimeout(this.dancer.step, timeBetweenSteps);
   };
-  dancer.step();
+  this.step(5);
 
-  dancer.setPosition = function(top, left) {
+  makeDancer.prototype.setPosition = function(top, left) {
     // Use css top and left properties to position our <span> tag
     // where it belongs on the page. See http://api.jquery.com/css/
     //
     var styleSettings = {
       top: top,
-      left: left
+      left: left,
     };
-    dancer.$node.css(styleSettings);
+    this.dancer.$node.css(styleSettings);
   };
 
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
-  dancer.setPosition(top, left);
+  this.setPosition(top, left);
 
-  return dancer;
-};
